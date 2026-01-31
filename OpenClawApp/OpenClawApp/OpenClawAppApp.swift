8fe632b0a10b142881fb26a4f8d7efb0bdc7ec53
Harro Krog <krogharro@gmail.com>
2026-01-31 12:29:52 +0100

    init ios app

---
+++ OpenClawApp/OpenClawApp/OpenClawAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenClawAppApp.swift
+//  OpenClawApp
+//
+//  Created by Harro Krog on 31.01.26.
+//
+
+import SwiftUI
+
+@main
+struct OpenClawAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
